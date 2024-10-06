a40fb30cdb3c53474213ece047f2acf452173854
jktham-sf <jonas.tham@gmx.ch>
2024-10-06 23:05:27 +0200

    get playback state and draw ui

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::{stdin, stdout}, time::{Duration, SystemTime, UNIX_EPOCH}};
+use std::{io::{stdin, stdout, Write}, time::{Duration, SystemTime, UNIX_EPOCH}};
 use crossterm::{cursor, event::{self, Event, KeyCode, KeyEvent}, style::{self, Color, Stylize}, terminal, ExecutableCommand, QueueableCommand};
 use reqwest::{self, blocking::Client};
 use serde_json::{self, Value};
@@ -15,6 +15,16 @@ struct Creds {
     access_token: String,
     refresh_token: String,
     expires_at: u64,
+}
+
+struct Playback {
+    is_active: bool,
+    is_playing: bool,
+    title: String,
+    album: String,
+    artist: String,
+    progress: i64,
+    duration: i64,
 }
 
 fn main() -> Result<(), Err> {
@@ -46,16 +56,16 @@ fn main() -> Result<(), Err> {
     }
     println!("new creds: {creds:?}");
 
-    // terminal::enable_raw_mode()?;
+    terminal::enable_raw_mode()?;
     stdout().execute(cursor::Hide)?;
-    
-    // clear()?;
-    // draw_rect(0, 0, 20, 10, Color::Red)?;
-    // stdout().execute(cursor::MoveToNextLine(1))?;
-    // stdout().flush()?;
+    stdout().execute(terminal::EnterAlternateScreen)?;
 
+    let mut frame: i32 = 0;
     loop {
-        if event::poll(Duration::from_millis(500))? {
+        let playback = get_playback(&client, &creds)?;
+        draw_ui(&playback, &frame)?;
+
+        if event::poll(Duration::from_millis(1000))? {
             match event::read()? {
                 Event::Key(event) => input(event, &client, &creds),
                 Event::FocusGained => Ok(()),
@@ -66,6 +76,7 @@ fn main() -> Result<(), Err> {
             }?;
         }
 
+        frame += 1;
     }
 }
 
@@ -81,6 +92,72 @@ fn input(event: KeyEvent, client: &Client, creds: &Creds) -> Result<(), Err> {
     return Ok(());
 }
 
+fn draw_ui(playback: &Playback, frame: &i32) -> Result<(), Err> {
+    let bg: Color = Color::Red;
+    let fg: Color = match (playback.is_active, playback.is_playing) {
+        (false, _) => Color::Red,
+        (true, false) => Color::Cyan,
+        (true, true) => Color::Green,
+    };
+
+    clear()?;
+    draw_rect(0, 0, 50, 7, bg)?;
+
+    let indicator: &str = match frame % 2 {
+        0 => "/",
+        1 => "\\",
+        _ => "",
+    };
+    draw_text(48, 1, indicator, fg)?;
+
+    let state: &str = match (playback.is_active, playback.is_playing) {
+        (false, _) => "inactive",
+        (true, false) => "paused",
+        (true, true) => "playing",
+    };
+    draw_text(2, 1, state, fg)?;
+
+    if playback.is_active {
+        draw_text(2, 2, &format!("{}:{:0>2} / {}:{:0>2}", &playback.progress/1000/60, &playback.progress/1000%60, &playback.duration/1000/60, &playback.duration/1000%60), fg)?;
+        draw_text(2, 4, &playback.title, fg)?;
+        draw_text(2, 5, &playback.album, fg)?;
+        draw_text(2, 6, &playback.artist, fg)?;
+    }
+
+    stdout().flush()?;
+    return Ok(());
+}
+
+fn get_playback(client: &Client, creds: &Creds) -> Result<Playback, Err> {
+    let res = client.get("https://api.spotify.com/v1/me/player")
+        .bearer_auth(&creds.access_token)
+        .send()?
+        .text()?
+    ;
+
+    let v: Value = serde_json::from_str(&res).unwrap_or(Value::String("inactive".to_string()));
+
+    let mut playback = Playback {
+        is_active: false,
+        is_playing: false,
+        title: "".to_string(),
+        album: "".to_string(),
+        artist: "".to_string(),
+        progress: 0,
+        duration: 0,
+    };
+    
+    playback.is_active = v.as_str().unwrap_or("") != "inactive";
+    playback.is_playing =  v["is_playing"].as_bool().unwrap_or(false);
+    playback.title = v["item"]["name"].as_str().unwrap_or("").to_string();
+    playback.album = v["item"]["album"]["name"].as_str().unwrap_or("").to_string();
+    playback.artist = v["item"]["artists"][0]["name"].as_str().unwrap_or("").to_string();
+    playback.progress = v["progress_ms"].as_i64().unwrap_or(0);
+    playback.duration = v["item"]["duration_ms"].as_i64().unwrap_or(0);
+
+    return Ok(playback);
+}
+
 fn new_auth(client: &Client, creds: &Creds) -> Result<Creds, Err> {
     let scope = "user-read-playback-state%20user-modify-playback-state%20user-read-currently-playing%20user-library-read";
     let redirect_uri = "https://localhost:8888/callback";
@@ -91,7 +168,7 @@ fn new_auth(client: &Client, creds: &Creds) -> Result<Creds, Err> {
     let mut input = String::new();
     stdin().read_line(&mut input)?;
 
-    let code = input.strip_prefix("https://localhost:8888/callback?code=").unwrap_or(&input).trim();
+    let code = input.strip_prefix(&format!("{redirect_uri}?code=")).unwrap_or(&input).trim();
     println!("code: {code}");
 
     let res = client.post("https://accounts.spotify.com/api/token")
@@ -163,7 +240,7 @@ fn test_request_playback(client: &Client, creds: &Creds) -> Result<(), Err> {
         .text()?
     ;
 
-    let v: Value = serde_json::from_str(&res).unwrap_or(Value::String("no playback".to_string()));
+    let v: Value = serde_json::from_str(&res).unwrap_or(Value::String("inactive".to_string()));
     println!("test_request_playback: {v}");
 
     return Ok(());
@@ -174,10 +251,10 @@ fn clear() -> Result<(), Err> {
     return Ok(());
 }
 
-fn draw_char(x: u16, y: u16, c: char, color: Color) -> Result<(), Err> {
+fn draw_text(x: u16, y: u16, t: &str, color: Color) -> Result<(), Err> {
     stdout()
         .queue(cursor::MoveTo(x, y))?
-        .queue(style::PrintStyledContent(c.with(color)))?
+        .queue(style::PrintStyledContent(t.with(color)))?
     ;
     return Ok(());
 }
@@ -185,35 +262,31 @@ fn draw_char(x: u16, y: u16, c: char, color: Color) -> Result<(), Err> {
 fn draw_rect(x1: u16, y1: u16, x2: u16, y2: u16, color: Color) -> Result<(), Err> {
     for x in x1..=x2 {
         for y in y1..=y2 {
-            if x == x1 || x == x2 || y == y1 || y == y2 {
-                let c: &str;
-                if x == x1 && y == y1 {
-                    c = "┌";
-                } else if x == x1 && y == y2 {
-                    c = "└";
-                } else if x == x2 && y == y1 {
-                    c = "┐";
-                } else if x == x2 && y == y2 {
-                    c = "┘";
-                } else if x == x1 || x == x2 {
-                    c = "│";
-                } else if y == y1 || y == y2 {
-                    c = "─";
-                } else {
-                    c = " ";
-                }
+            let c: &str = match (x, y) {
+                (x, y) if (x, y) == (x1, y1) => "┌",
+                (x, y) if (x, y) == (x1, y2) => "└",
+                (x, y) if (x, y) == (x2, y1) => "┐",
+                (x, y) if (x, y) == (x2, y2) => "┘",
+                (x, _) if x == x1 || x == x2 => "│",
+                (_, y) if y == y1 || y == y2 => "─",
+                (_, _) => "",
+            };
+
+            if c != "" {
                 stdout()
                     .queue(cursor::MoveTo(x, y))?
                     .queue(style::PrintStyledContent(c.with(color)))?
                 ;
             }
+
         }
     }
     return Ok(());
 }
 
 fn quit() -> Result<(), Err> {
-    terminal::disable_raw_mode()?;
+    stdout().execute(terminal::LeaveAlternateScreen)?;
     stdout().execute(cursor::Show)?;
+    terminal::disable_raw_mode()?;
     std::process::exit(0);
 }
